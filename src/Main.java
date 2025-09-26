1b298ea5e871887a1b613e11f869d513e774b012
ZaydenMartires <zaydenm3@nycstudents.net>
2025-09-26 08:35:46 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,42 +5,52 @@ public class Main
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("------------------------------------------");
-        System.out.print("Enter your Bill: $");
+        //the prompts for the user inputs are here
+        System.out.print("Enter your Bill: $");//bill
         double billAmt = scan.nextDouble();
-        System.out.print("Tip Percent: %");
+        System.out.print("Tip Percent: %"); //tip percent
         int tipPerc= scan.nextInt();
-        System.out.print("# of People:");
+        System.out.print("# of People:"); //# of people
         int amtPeople = scan.nextInt();
+
         System.out.println("---------------------");
-        double totalTipAmt = billAmt * (tipPerc/100.0);
-        double roundedTipAmt = Math.round(totalTipAmt * 100.0) / 100.0;
+//the tip amount
+        double totalTipAmt = billAmt * (tipPerc/100.0); //The actual math is at the start of each "Section" of code
+        double roundedTipAmt = Math.round(totalTipAmt * 100.0) / 100.0; //rounding did not work well with just math round so I had to format it too
         System.out.printf("| Total tip Amt: $%.2f", roundedTipAmt);
         System.out.print(" |");
         System.out.println();
+
         System.out.println("---------------------");
-        double totalBillAmt= billAmt + totalTipAmt;
-        double roundedBillAmt = Math.round(totalBillAmt * 100.0) / 100.0;
-        System.out.printf("| Total Bill Amt: $%.2f", roundedBillAmt );
+//Total bill amount
+        double totalBillAmt= billAmt + totalTipAmt; // The rest of the code from here takes the user input AND calculations
+        double roundedBillAmt = Math.round(totalBillAmt * 100.0) / 100.0;//rounding
+        System.out.printf("| Total Bill Amt: $%.2f", roundedBillAmt ); //formating
         System.out.print(" |");
-        System.out.println();
+        System.out.println();//line break
+
         System.out.println("---------------------");
+//The tip per person
         double tipPerPerson = totalTipAmt/amtPeople;
         double roundedTipPerPerson = Math.round(tipPerPerson * 100.0) / 100.0;
         System.out.printf("| Tip Per Person: $%.2f",roundedTipPerPerson);
         System.out.print(" |");
-        System.out.println();
+        System.out.println();// since I used "System.out.printf" I couldn't make a new line, so I had to add the line break here
+
         System.out.println("---------------------");
+//the total bill for each person
         double billPerPerson = totalBillAmt/amtPeople;
         double roundedPerBillAmt = Math.round(billPerPerson * 100.0) / 100.0;
         System.out.printf("| Total Bill per Person: $%.2f", roundedPerBillAmt );
-        System.out.print(" |");
+        System.out.print(" |");//this is just to make it look nice on each of the "sections"
         System.out.println();
+
         System.out.println("---------------------");
 
 
 
 
-        scan.close();
+        scan.close(); //I closed it down here in case I needed to make changes
 
     }
     }
